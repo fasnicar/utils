9c2357e57524cc0b6dfcd5960cc595cbdd6588b0
Francesco Asnicar <francescoasnicar@gmail.com>
2015-04-02 14:03:02 +0200

    sub_taxa.py is a simple script that take a newick tree and a taxonomic file and add to the genome id in the tree file the taxonomic information

---
+++ sub_taxa.py
@@ -0,0 +1,19 @@
+from sys import argv
+from subprocess import call
+
+tree = argv[1]
+taxa = argv[2]
+
+print "Tree file:", tree
+print "Taxa file:", taxa
+
+taxad = {}
+with open(taxa, 'r') as f:
+    for r in f.readlines()[1:]:
+        gid = r.split('\t')[0]
+        taf = r.split('\t')[31]
+        taxad[gid] = taf
+
+for gid, taf in taxad.iteritems():
+    call(['sed', '-i', 's/'+gid+'/'+gid+'_'+taf+'/g', tree])
+
