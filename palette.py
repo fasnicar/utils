6d99259ee646ae4467c08f1c1f9976d46ac65766
Francesco Asnicar <francescoasnicar@gmail.com>
2015-05-28 17:35:28 +0200

    palette.py, start coding our beautiful palette generator

---
+++ palette.py
@@ -0,0 +1,13 @@
+# imports
+
+
+__date__ = '26 May 2015'
+__email__ = 'f.asnicar@unitn.it'
+__author__ = 'Francesco Asnicar'
+__version__ = '0.01'
+
+
+def get_palette():
+    """
+    """
+    return
