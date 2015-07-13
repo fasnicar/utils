954047bb9fb8ad0060f553dc892be9a9d36936e3
Francesco Asnicar <francescoasnicar@gmail.com>
2015-07-13 10:00:22 +0200

    fixed reading

--- sub_taxa.py
+++ sub_taxa.py
@@ -14,9 +14,11 @@ print "Taxa file:", taxa
 taxad = {}
 with open(taxa, 'r') as f:
     for r in f.readlines()[1:]:
-        gid = r.split('\t')[0]
+        gid = r.split('\t')[0].strip()
         taf = r.split('\t')[31]
+        # taf = r.split('\t')[1].strip()
         taxad[gid] = taf
 
 for gid, taf in taxad.iteritems():
     call(['sed', '-i', 's/'+gid+'/'+gid+'_'+taf+'/g', tree])
+    # print(' '.join(['sed', '-i', '"s/'+gid+'/'+gid+'_'+taf+'/g"', tree]))
