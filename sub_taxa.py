141bea56972536332d74a28115d02b9e1731863b
Francesco Asnicar <francescoasnicar@gmail.com>
2015-08-04 14:48:08 +0200

    ask the col index to cut, check input params

--- sub_taxa.py
+++ sub_taxa.py
@@ -5,18 +5,23 @@ from sys import argv
 from subprocess import call
 
 
-tree = argv[1]
-taxa = argv[2]
+try:
+    tree = argv[1]
+    taxa = argv[2]
+    rtaxa = int(argv[3])
+except:
+    print "py utils.py <tree_file> <taxa_file> <taxa_row>"
+    print "    taxa_row should be 1 for phylophlan/data/ppafull.tax.txt or 31 for phylophlan/input/bigbigtree_taxa.txt"
 
 print "Tree file:", tree
 print "Taxa file:", taxa
+print "Taxa row:", rtaxa
 
 taxad = {}
 with open(taxa, 'r') as f:
     for r in f.readlines()[1:]:
         gid = r.split('\t')[0].strip()
-        taf = r.split('\t')[31]
-        # taf = r.split('\t')[1].strip()
+        taf = r.split('\t')[rtaxa].strip()
         taxad[gid] = taf
 
 for gid, taf in taxad.iteritems():
