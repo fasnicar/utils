d7c69e3e911382e455dadc7dadb1a1493deb4d67
Francesco Asnicar <francescoasnicar@gmail.com>
2015-08-06 11:44:39 +0200

    start coding shared_bugs.py, input is a metaphlan merged table and a list of columns, output is the bug list shared between the columns

---
+++ shared_bugs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+
+from sys import argv
+
+
+try:
+    tsv = str(argv[1])
+    cols = [int(i) for i in argv[2].split(',')]
+    outf = str(argv[3])
+except:
+    print "py shared_bugs.py <tsv-mp2-merge> <colX>,<colY>,.. <output-name>"
+    print "py shared_bugs.py all.txt 3,6 all.png"
+    print "cols indexes start from 1!"
+    exit(1)
+
+labels = []
+bugs_list = {}
+header = True
+
+with open(tsv) as f:
+    for r in f:
+        if header:
+            header = False
+
+            for c in cols:
+                labels.append(r.strip().split()[c-1])
+        else:
+            bug = str(r.strip().split()[0])
+            bugs_list[bug] = []
+
+            for c in cols:
+                bugs_list[bug].append(float(r.strip().split()[c-1]))
+
+sep = '\t' if outf.endswith('.tsv') else ','
+
+with open(outf, 'w') as f:
+    f.write(sep.join(['bugs'] + labels) + '\n')
+
+    for bug, abus in bugs_list.iteritems():
+        if len([abu for abu in abus if abu > 0]) == len(abus):
+            f.write(sep.join([bug] + [str(abu) for abu in abus]) + '\n')
