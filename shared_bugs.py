53a417d2463006867830746477d30592f5eca654
Francesco Asnicar <francescoasnicar@gmail.com>
2015-08-06 12:12:51 +0200

    shared_bugs.py updated, take also list of row indexs to skip

--- shared_bugs.py
+++ shared_bugs.py
@@ -7,30 +7,36 @@ from sys import argv
 try:
     tsv = str(argv[1])
     cols = [int(i) for i in argv[2].split(',')]
-    outf = str(argv[3])
+    rows = [int(i) for i in argv[3].split(',')]
+    outf = str(argv[4])
 except:
-    print "py shared_bugs.py <tsv-mp2-merge> <colX>,<colY>,.. <output-name>"
+    print "py shared_bugs.py <tsv-mp2-merge> <col1,col2,..> <row1,row2,..> <output-name>"
     print "py shared_bugs.py all.txt 3,6 all.png"
-    print "cols indexes start from 1!"
+    print "cols and rows indexes start from 1!"
     exit(1)
 
 labels = []
 bugs_list = {}
 header = True
+row_count = 1
+sep = ',' if tsv.endswith('.csv') else '\t'
 
 with open(tsv) as f:
     for r in f:
-        if header:
-            header = False
+        if row_count not in rows:
+            if header:
+                header = False
 
-            for c in cols:
-                labels.append(r.strip().split()[c-1])
-        else:
-            bug = str(r.strip().split()[0])
-            bugs_list[bug] = []
+                for c in cols:
+                    labels.append(r.strip().split(sep)[c-1])
+            else:
+                bug = str(r.strip().split(sep)[0])
+                bugs_list[bug] = []
 
-            for c in cols:
-                bugs_list[bug].append(float(r.strip().split()[c-1]))
+                for c in cols:
+                    bugs_list[bug].append(float(r.strip().split(sep)[c-1]))
+
+        row_count += 1
 
 sep = '\t' if outf.endswith('.tsv') else ','
 
