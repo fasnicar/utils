0215faa0e61384b88caac3302619cc0be3142e27
Francesco Asnicar <francescoasnicar@gmail.com>
2015-08-26 08:54:17 +0200

    sub_taxa.py improved

--- sub_taxa.py
+++ sub_taxa.py
@@ -2,26 +2,28 @@
 
 
 from sys import argv
+from sys import exit
 from subprocess import call
 
 
 try:
     tree = argv[1]
     taxa = argv[2]
-    rtaxa = int(argv[3])
+    ctaxa = int(argv[3])
 except:
-    print "py utils.py <tree_file> <taxa_file> <taxa_row>"
-    print "    taxa_row should be 1 for phylophlan/data/ppafull.tax.txt or 31 for phylophlan/input/bigbigtree_taxa.txt"
+    print "py utils.py <tree_file> <taxa_file> <taxa_col>"
+    print "    taxa_col should be 1 for phylophlan/data/ppafull.tax.txt or 31 for phylophlan/input/bigbigtree_taxa.txt"
+    exit(1)
 
 print "Tree file:", tree
 print "Taxa file:", taxa
-print "Taxa row:", rtaxa
+print "Taxa col:", ctaxa
 
 taxad = {}
 with open(taxa, 'r') as f:
     for r in f.readlines()[1:]:
         gid = r.split('\t')[0].strip()
-        taf = r.split('\t')[rtaxa].strip()
+        taf = r.split('\t')[ctaxa].strip()
         taxad[gid] = taf
 
 for gid, taf in taxad.iteritems():
