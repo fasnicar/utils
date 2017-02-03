a4206a90fc4e7cbd54ddabbbee9d163ab7c08312
francesco-asnicar <francesco.asnicar@gmail.com>
2017-02-03 15:46:02 +0100

    added the possibility to print the palette, added seed param to re-generate the same palette

--- palette.py
+++ palette.py
@@ -1,17 +1,27 @@
-from colorsys import hsv_to_rgb
+#! /usr/bin/env python
 
 
-__date__ = '26 May 2015'
+import colorsys
+import random
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+sns.set()
+
+
+__date__ = '14 Dec 2016'
 __email__ = 'f.asnicar@unitn.it'
 __author__ = 'Francesco Asnicar'
-__version__ = '0.01'
+__version__ = '0.02'
 
 
-def scale_color((h, s, v), factor=1.):
+def scale_color(hsv, factor=1.0):
     """
     Takes as input a tuple that represents a color in HSV format, and optionally a scale factor.
     Return an RGB string that is the converted HSV color, scaled by the given factor.
     """
+    h, s, v = hsv
+
     if (h < 0.) or (h > 360.):
         raise Exception('[scale_color()] Hue value out of range (0, 360): ' + str(h))
 
@@ -20,34 +30,42 @@ def scale_color((h, s, v), factor=1.):
 
     if (v < 0.) or (v > 100.):
         raise Exception('[scale_color()] Value value out of range (0, 100): ' + str(v))
-
     if (factor < 0.) or (factor > 1.):
         raise Exception('[scale_color()] Factor value out of range (0.0, 1.0): ' + str(factor))
 
     v *= factor
-    r, g, b = hsv_to_rgb(h/360., s/100., v/100.)
+    r, g, b = colorsys.hsv_to_rgb(h/360., s/100., v/100.)
 
     return '#{0:02x}{1:02x}{2:02x}'.format(int(round(r*255.)), int(round(g*255.)), int(round(b*255.)))
 
 
-def get_palette(num_colors):
+def get_palette(num_colors, my_seed=None):
     """
     """
     colorss = []
-    step = 33.
+    step = 36.0
 
-    if num_colors < 10:
-        step = 360. / ((num_colors+1) / 2)
+    random.seed(my_seed)
 
-    hue = 10.
-    nsteps = 1
+    if num_colors < 20:
+        step = 360.0 / ((num_colors+1) / 2)
+
+    hue = random.random()*360.0
+
     while len(colorss) < num_colors:
-        if hue + step > 360.:
-            nsteps += 1
-            hue = (nsteps**3) + 10.
+        if hue + step > 360.0:
+            hue = random.random()*360.0
 
-        colorss.append(scale_color((hue, 66., 99.)))
-        colorss.append(scale_color((hue, 77., 45.)))
+        colorss.append(scale_color((hue, 89.0, 100.0)))
+        colorss.append(scale_color((hue, 100.0, 58.0)))
         hue += step
 
+    random.shuffle(colorss)
     return colorss[:num_colors]
+
+
+def print_palette(palette, output_filename):
+    """
+    """
+    plot = sns.palplot(palette)
+    plot.savefig(output_filename)
